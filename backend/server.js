e8d004dc8138358ada0447e03f60d20745f39b25
KirthigaManivannan <148140488+KirthigaManivannan@users.noreply.github.com>
2024-12-21 22:44:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,5 +39,5 @@ app.all('*', (req, res) => {
 });
 
 // Start the Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
