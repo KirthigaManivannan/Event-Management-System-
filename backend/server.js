c9621e847e7e8d6970b1089f3afe005657da4361
KirthigaManivannan <148140488+KirthigaManivannan@users.noreply.github.com>
2024-12-25 13:01:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const port = process.env.PORT || 5000;
 
 // Load environment variables from the .env file
 dotenv.config();
